8aef33e43bdcab7cc6535e9d342b94dc81a61e23
Yuhtin <rio.daviduarte@gmail.com>
2021-11-04 14:40:10 -0300

    Initial commit

---
+++ src/main/java/com/yuhtin/commission/afelia/commandshop/AfeliaCommandShop.java
@@ -0,0 +1,12 @@
+package com.yuhtin.commission.afelia.commandshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AfeliaCommandShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+}
---
+++ src/main/java/com/yuhtin/commission/afelia/commandshop/command/ShopCommand.java
@@ -0,0 +1,14 @@
+package com.yuhtin.commission.afelia.commandshop.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class ShopCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+}
